2e853135efd2fa8c89de4520adcb17b64c12c638
Yoshi Miyakawa <ymiyakawa@lcs.on.ca>
2024-06-03 14:27:46 -0400

    got a fixed version of intro view mostly done

--- MedsReminder/MedsReminderApp.swift
+++ MedsReminder/MedsReminderApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct MedsReminderApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            IntroView()
         }
     }
 }
---
+++ MedsReminder/Views/IntroView.swift
@@ -0,0 +1,50 @@
+//
+//  IntroView.swift
+//  MedsReminder
+//
+//  Created by 宮川義之助 on 2024/06/03.
+//
+
+import SwiftUI
+
+struct IntroView: View {
+    var body: some View {
+        ZStack {
+            Color.specialGreen
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                Text("Meds\nReminder")
+                    .foregroundStyle(Color.white)
+                    .font(.largeTitle)
+                    .bold()
+                Spacer()
+                Image("IntroImage")
+                    .resizable()
+                    .scaledToFit()
+                    .padding(.horizontal, 50)
+                Spacer()
+                
+                NavigationLink {
+                    Text("another view")
+                } label: {
+                    Text("Get Started")
+                        .underline()
+                        .foregroundStyle(Color.white)
+                        .font(.largeTitle)
+                        .bold()
+
+                }
+
+
+                Spacer()
+
+            }
+        }
+    }
+}
+
+#Preview {
+    IntroView()
+}
