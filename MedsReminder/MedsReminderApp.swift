a185f39c88bc633e9257795a683e587b5d85fa35
Yoshi Miyakawa <ymiyakawa@lcs.on.ca>
2024-06-03 13:30:18 -0400

    Initial Commit

---
+++ MedsReminder/MedsReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedsReminderApp.swift
+//  MedsReminder
+//
+//  Created by 宮川義之助 on 2024/06/03.
+//
+
+import SwiftUI
+
+@main
+struct MedsReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
